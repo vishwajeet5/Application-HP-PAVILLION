d81ce2cbe583a4cb34f693093bff85ba7681a7ca
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2023-08-23 19:56:35 +0530

    your

---
+++ java/frog_jump.java
@@ -0,0 +1,16 @@
+public class frog_jump{
+    static int best(int[] h, int n , int idx){
+    // base case 
+    if (idx == n-1) return 0;
+    // recursion work
+    int op1 = Math.abs(h[idx] - h[idx+1]) + best(h,n,idx+1);
+    if (idx == n-2) return op1;
+    int op2 = Math.abs(h[idx] - h[idx+2]) + best(h,n,idx+2);
+    // self work
+    return Math.min(op1, op2);
+    }
+    public static void main(String[] args) {
+        int [] h = {10,30,40,20};
+        System.out.println(best(h, h.length, 0));
+    }
+}
