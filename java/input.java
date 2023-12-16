acfb47c5902b976bbce931855ec6cab134a4349a
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2023-12-16 12:45:19 +0530

    l

--- java/input.java
+++ java/input.java
@@ -1,5 +1,10 @@
-public class input{
-    public static void main(String[] args) {
-        System.out.println("hiii vishwajeet");
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+            String i = sc.nextLine();
+
+            System.out.println("float value " +i);
     }
-} 
+}
