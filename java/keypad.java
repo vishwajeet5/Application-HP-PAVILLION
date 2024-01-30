58788dc31fa5f810f650a291d81775dedeeb0fe6
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-01-30 21:30:10 +0530

    y

---
+++ java/keypad.java
@@ -0,0 +1,19 @@
+public class keypad {
+    static void combination(String dig, String[] kp, String res){
+        if dig.length() == 0{
+            System.out.print(res + " ");
+            return;
+        }
+        int currNum = dig.charAt(0)- 0; //2
+        String cirrChoices = kp[currNum];  //"abc"
+        for(int i = 0; i < cirrChoices.length();i++){
+            combination(dig.substring(1), kp, res + cirrChoices.charAt(i));
+        }
+    }
+    public static void main(String[] args) {
+        String s = "23";
+        // kp[7] = "pqrs"
+        String[] kp = {"", "", "abc", "def", "ghi","jkl", "mno","pqrs","tuv","wxyz"};
+        //              0   1   2       3       4     5     6     7      8      9
+    }
+}
