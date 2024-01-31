ac45e42b99a64bb0bd1286118ae9e51202ac314f
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-01-31 23:02:44 +0530

    hh

--- java/lexicographicalMethod.java
+++ java/lexicographicalMethod.java
@@ -1,6 +1,24 @@
 public class lexicographicalMethod {
+
+    static void sortFruit(String[] fruits){
+        int n = fruits.length;
+        for(int i= 0; i<n-1 ; i++){
+            int min_index = i;
+            for (int j = i+1; j<n;j++){
+                if (fruits[j].compareTo(fruits[min_index])<0){
+                    min_index = j;
+                }
+            }
+            String temp = fruits[i];
+            fruits[i] = fruits[min_index];
+            fruits[min_index] = temp;
+        }
+    }
     public static void main (String [] args){ 
         String[] fruits = {"kiwi" , "apple" , "papaya" , "mango"};
-        
+        sortFruit(fruits);
+        for(String val : fruits){
+             System.out.print(val + " ");
+        }
     }
 }
