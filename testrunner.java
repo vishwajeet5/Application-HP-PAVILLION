c0f5e759e456d7cd726a3aadceb2096cc80f4b57
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-05-13 18:43:39 +0530

    k

--- testrunner.java
+++ testrunner.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 public class testrunner {
-    public static void main(String[] args) {
+   /*  public static void main(String[] args) {
         // Creating an array of integers
         int[] numbers = { 1, 2, 3, 4, 5 };
 
@@ -21,6 +22,35 @@ public class testrunner {
         for (int num : numbers) {
             System.out.println(num);
         }
+    }*/
+
+    // @SuppressWarnings("resource")
+    public static void main (String arg[]){
+        try (// @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in)) {
+            int N = sc.nextInt();
+
+            // Convert the number from desimal to binary
+            String binary = Integer.toBinaryString(N);
+
+            // Toggle the bits
+            StringBuilder st = new StringBuilder();
+            for (char i: binary.toCharArray()){
+                if (i == '1'){
+                    st.append('0');
+                }else {
+                    st.append('1');
+                }
+            }
+
+            // Convert the number from binary to desimal
+            int result = Integer.parseInt(st.toString(),2);
+
+            System.out.println(result);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
