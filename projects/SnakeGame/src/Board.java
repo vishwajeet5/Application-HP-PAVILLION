eb56664b8d26f9747b474d2615ffe9ed1b0cd02c
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <kumarvishwajeet567@gmail.com>
2024-06-18 18:14:09 +0530

    .

--- projects/SnakeGame/src/Board.java
+++ projects/SnakeGame/src/Board.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
-
 public class Board extends JPanel implements ActionListener {
 
     private Image apple;
@@ -17,18 +15,16 @@ public class Board extends JPanel implements ActionListener {
     private int apple_x;
     private int apple_y;
 
-    private final int x[] = new int [ALL_DOTS];
+    private final int x[] = new int[ALL_DOTS];
     private final int y[] = new int[ALL_DOTS];
 
     private boolean upDirection = false;
     private boolean downDirection = false;
     private boolean rightDirection = true;
     private boolean leftDirection = false;
-    
 
     private int dots;
     private Timer timer;
-
 
     Board() {
 
@@ -41,7 +37,7 @@ public class Board extends JPanel implements ActionListener {
         initGame();
     }
 
-    public void loadImages(){
+    public void loadImages() {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/snakedot.jpg"));
         dot = i1.getImage();
 
@@ -49,17 +45,16 @@ public class Board extends JPanel implements ActionListener {
         apple = i2.getImage();
 
         ImageIcon i3 = new ImageIcon(ClassLoader.getSystemResource("icon/snakehead.jpg"));
-        head =i3.getImage();
-
+        head = i3.getImage();
 
     }
 
-    public void initGame(){
+    public void initGame() {
         dots = 3;
 
-        for(int i = 0; i<dots; i++){
-            y[i]= 50;
-            x[i] = 50 - i*DOT_SIZE;
+        for (int i = 0; i < dots; i++) {
+            y[i] = 50;
+            x[i] = 50 - i * DOT_SIZE;
         }
 
         locateapple();
@@ -68,90 +63,93 @@ public class Board extends JPanel implements ActionListener {
         timer.start();
     }
 
-    public void locateapple(){
-        int r = (int)(Math.random() * RANDOM_POSITION);
-            apple_x = r * DOT_SIZE;
+    public void locateapple() {
+        int r = (int) (Math.random() * RANDOM_POSITION);
+        apple_x = r * DOT_SIZE;
 
-        r = (int)(Math.random() * RANDOM_POSITION);
-            apple_y = r * DOT_SIZE;
+        r = (int) (Math.random() * RANDOM_POSITION);
+        apple_y = r * DOT_SIZE;
 
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         draw(g);
     }
 
-    public void draw(Graphics g){
+    public void draw(Graphics g) {
 
         g.drawImage(apple, apple_x, apple_y, this);
 
-        for (int i = 0; i<dots; i++){
-            if (i == 0){
+        for (int i = 0; i < dots; i++) {
+            if (i == 0) {
                 g.drawImage(head, x[i], y[i], this);
-            }else{
-                    g.drawImage(dot, x[i], y[i], this);
-                }
-            }
-
-            Toolkit.getDefaultToolkit().sync();
-
-        }
-
-        public void move(){
-            for (int i = dots;i> 0 ; i--){
-                x[i] = x[i - 1];
-                y[i] = y[i - 1];
-            }
-
-            if (leftDirection){
-                x[0] = x[0] - DOT_SIZE;
-            }if (rightDirection){
-                x[0] = x[0] + DOT_SIZE;
-            }if (upDirection){
-                y[0] = y[0] - DOT_SIZE;
-            }if (downDirection){
-                y[0] = y[0] + DOT_SIZE;
-            }
-
-            x[0] += DOT_SIZE;
-            y[0] += DOT_SIZE;
-
-        }
-
-        public void actionPerformed(ActionEvent e){
-            move();
-
-            repaint();
-        }
-
-        public class TAdapter extends KeyAdapter{
-            
-            public void keyPressed(KeyEvent e){
-                int key = e.getKeyCode();
-
-                if (key == KeyEvent.VK_LEFT && (!rightDirection)){
-                    leftDirection = true;
-                    upDirection = false;
-                    downDirection = false;
-                }
-                if (key == KeyEvent.VK_RIGHT && (!leftDirection)){
-                    rightDirection = true;
-                    upDirection = false;
-                    downDirection = false;
-                }
-                if (key == KeyEvent.VK_UP && (!downDirection)){
-                    leftDirection = false;
-                    upDirection = true;
-                    rightDirection = false;
-                }
-                if (key == KeyEvent.VK_DOWN && (!upDirection)){
-                    leftDirection = false;
-                    downDirection = true;
-                    rightDirection = false;
-                }
-
+            } else {
+                g.drawImage(dot, x[i], y[i], this);
             }
         }
+
+        Toolkit.getDefaultToolkit().sync();
+
+    }
+
+    public void move() {
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+
+        if (leftDirection) {
+            x[0] = x[0] - DOT_SIZE;
+        }
+        if (rightDirection) {
+            x[0] = x[0] + DOT_SIZE;
+        }
+        if (upDirection) {
+            y[0] = y[0] - DOT_SIZE;
+        }
+        if (downDirection) {
+            y[0] = y[0] + DOT_SIZE;
+        }
+
+        x[0] += DOT_SIZE;
+        y[0] += DOT_SIZE;
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        move();
+
+        repaint();
+    }
+
+    public class TAdapter extends KeyAdapter {
+
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+
+            if (key == KeyEvent.VK_LEFT && (!rightDirection)) {
+                leftDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+            if (key == KeyEvent.VK_RIGHT && (!leftDirection)) {
+                rightDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+            if (key == KeyEvent.VK_UP && (!downDirection)) {
+                leftDirection = false;
+                upDirection = true;
+                rightDirection = false;
+            }
+            if (key == KeyEvent.VK_DOWN && (!upDirection)) {
+                leftDirection = false;
+                downDirection = true;
+                rightDirection = false;
+            }
+
+        }
+    }
 }
--- projects/SnakeGame/src/SnakeGame.java
+++ projects/SnakeGame/src/SnakeGame.java
@@ -2,20 +2,19 @@ import javax.swing.JFrame;
 
 public class SnakeGame extends JFrame {
 
-    SnakeGame(){
+    SnakeGame() {
         super("Snake Game");
         add(new Board());
         pack();
-        setSize(300 , 300);
+        setSize(300, 300);
         setLocationRelativeTo(null);
 
         setVisible(true);
-        
+
     }
 
     public static void main(String[] args) throws Exception {
-        new SnakeGame ();
+        new SnakeGame();
 
-        
     }
 }
