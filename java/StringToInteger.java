9468d11d4dd3f65f63f7ede6fdeaa57810e0e49b
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-06-29 13:47:35 +0530

    string to integer exception check

--- java/StringToInteger.java
+++ java/StringToInteger.java
@@ -1,13 +1,29 @@
 import java.util.*;
 
 public class StringToInteger{
-    public static void main(String[] args){
+    /*public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
-        char c = sc.next().charAt(0);
-        int n = c;
-        System.out.println(n);
-        
+        try {
+            int n = sc.nextInt();
+            // int n = Integer.parseInt(s);
+            System.out.println(n);
+        } 
+            catch (Exception e) {
+            System.out.println("Bad String");
+        }
+*/
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            String s = sc.nextLine();
+            try {
+                int n = Integer.parseInt(s);
+                System.out.println(n);
+            } 
+            catch (NumberFormatException e) {
+                System.out.println("Bad String");
+            }
+    
         sc.close();
 
     }
