a0a30d026a163e2f21e555c1e34bcbc2b2d3f17c
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-06-30 21:05:28 +0530

    17

---
+++ java/moreException17.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class moreException17 {
+
+    public static void powerCalc(int n, int p){
+        System.out.println((int)(Math.pow(n, p)));
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++){
+            int n = sc.nextInt();
+            int p = sc.nextInt();
+
+            if (n >= 0 && p >= 0){
+                powerCalc(n, p);
+            }
+            else{
+                System.out.println("n and p should be non-negative");
+            }
+        }
+        sc.close();
+    }
+}
