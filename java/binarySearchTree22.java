7d6d766f53b4d98074803bce16336f7cf3ae665e
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-07-09 08:48:38 +0530

    25

--- java/binarySearchTree22.java
+++ java/binarySearchTree22.java
@@ -1,56 +1,52 @@
-
 import java.util.*;
 
-class Node{
-    Node left,right;
+class Node {
+    Node left, right;
     int data;
-    Node(int data){
-        this.data=data;
-        left=right=null;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
     }
 }
 
 public class binarySearchTree22 {
 
+    public static int getHeight(Node root) {
+        // Write your code here
 
-
-	public static int getHeight(Node root){
-      //Write your code here
-      
-        if(root == null){
+        if (root == null) {
             return -1;
-        }else{
-            return 1+Math.max(getHeight(root.left),getHeight(root.right));
+        } else {
+            return 1 + Math.max(getHeight(root.left), getHeight(root.right));
         }
     }
-    
 
-    public static Node insert(Node root,int data){
-        if(root==null){
+    public static Node insert(Node root, int data) {
+        if (root == null) {
             return new Node(data);
-        }
-        else{
+        } else {
             Node cur;
-            if(data<=root.data){
-                cur=insert(root.left,data);
-                root.left=cur;
-            }
-            else{
-                cur=insert(root.right,data);
-                root.right=cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
             }
             return root;
         }
     }
-	 public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int T=sc.nextInt();
-        Node root=null;
-        while(T-->0){
-            int data=sc.nextInt();
-            root=insert(root,data);
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        Node root = null;
+        while (T-- > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
         }
-        int height=getHeight(root);
+        int height = getHeight(root);
         System.out.println(height);
 
         sc.close();
---
+++ java/runningTimeAndComplaxity25.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class runningTimeAndComplaxity25 {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++){
+            int m = sc.nextInt();
+            int c = 0;
+            if (m == 1){
+                System.out.println("Not prime");
+            }else{
+                for(int j = 2; j < m; j++){
+                    if(m%j == 0){
+                        c ++;
+                    }
+                }
+                if(c == 0){
+                    System.out.println("Prime");
+                }
+                else{
+                    System.out.println("Not prime");
+                    c = 0;
+                }
+            }
+        } 
+        sc.close();
+    }
+}
