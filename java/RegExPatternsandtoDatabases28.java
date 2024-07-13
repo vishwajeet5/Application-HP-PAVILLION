17b51a9870c46d76130854849aa601f16adbd44a
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-07-13 14:12:34 +0530

    completed 30 days of code

---
+++ java/RegExPatternsandtoDatabases28.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.util.stream.IntStream;
+import java.io.*;
+
+public class RegExPatternsandtoDatabases28 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String m = "@gmail.com";
+        
+        List <String> a = new LinkedList<>();
+        
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, N).forEach(NItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                String firstName = firstMultipleInput[0];
+
+                String emailID = firstMultipleInput[1];
+                
+                if (emailID.contains(m)){
+                    a.add(firstName);
+                }
+                
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+            Collections.sort(a);
+            
+            for(String first:a){
+                System.out.println(first);
+            }
+        bufferedReader.close();
+    }
+}
---
+++ java/bitwiseAND29.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.stream.IntStream;
+
+
+
+    
+class Result {
+
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K) {
+    // Write your code here
+    int finalResult = 0;
+    for(int i = 1; i<N; i++){
+    for(int j = i+1; j <= N; j++){
+        int value = i&j;
+        if(value < K && value > finalResult){
+            finalResult = value;
+        }
+    }
+
+    }
+    return finalResult;
+}
+
+}
+public class bitwiseAND29 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int count = Integer.parseInt(firstMultipleInput[0]);
+
+                int lim = Integer.parseInt(firstMultipleInput[1]);
+
+                int res = Result.bitwiseAnd(count, lim);
+
+                bufferedWriter.write(String.valueOf(res));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
