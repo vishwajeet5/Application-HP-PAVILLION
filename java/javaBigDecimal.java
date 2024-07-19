10c82f344adc3ec6911b74856eb831f24cea3482
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-07-19 14:43:08 +0530

    java Big

---
+++ java/javaBigDecimal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.math.BigDecimal;
+
+public class javaBigDecimal {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        
+    }
+}
