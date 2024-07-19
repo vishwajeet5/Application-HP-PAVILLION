1d7e259f600a361b513d2bdc19df632027641e87
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-07-19 19:21:34 +0530

    decimal

--- java/javaBigDecimal.java
+++ java/javaBigDecimal.java
@@ -2,10 +2,17 @@ import java.util.*;
 import java.math.BigDecimal;
 
 public class javaBigDecimal {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        BigDecimal arr[] = new BigDecimal[n];
+
+        Arrays.sort(arr);
         
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        sc.close();
     }
 }
