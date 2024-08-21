389661c39299fa3dfae4b5a20977258a52e22a1c
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-08-21 18:18:30 +0530

    kadanes

--- java/maxSubArraySumKadane_sAlgorithm.java
+++ java/maxSubArraySumKadane_sAlgorithm.java
@@ -1,6 +1,18 @@
 public class maxSubArraySumKadane_sAlgorithm {
+    public static void kadanes(int arr[]){
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            cs += arr[i];
+            if (cs < 0){
+                cs = 0;
+            }
+            ms = Math.max(ms, cs);
+        }
+        System.out.println("our max sunarray sum is : "+ms);
+    }
     public static void main(String [] args){
         int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-        
+        kadanes(arr);
     }
 }
