dfe4cf71d8c39a7dbcf52f00de70bb689bffd360
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <114920830+vishwajeet5@users.noreply.github.com>
2024-09-07 10:32:52 +0530

    matrix

---
+++ java/searchInSortedMatrix.java
@@ -0,0 +1,24 @@
+public class searchInSortedMatrix {
+    public static boolean stairCaseSearch(int matrix[][], int key){
+        int row = 0, col = matrix[0].length-1;
+
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] == key){
+                System.out.println("Found key at (" + row + "," + col + ").");
+                return true;
+            }else if (key < matrix[row][col]){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        int matrix[][] = {{10, 20, 30, 40},{15, 25, 35, 45},{27, 29, 37, 48},{32, 33, 29, 50}};
+
+        int key = 33;
+
+        stairCaseSearch(matrix, key);
+    }
+}
