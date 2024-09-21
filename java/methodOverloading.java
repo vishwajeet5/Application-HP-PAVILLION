16fba542b0e734d02793b33851d5f10fd5b37cd9
ᐯ丨丂卄山卂ﾌ乇乇ㄒ Ҝㄩ爪卂尺 <kumarvishwajeet567@gmail.com>
2024-09-21 20:04:01 +0530

    method overriding and overloading

---
+++ java/methodOverloading.java
@@ -0,0 +1,18 @@
+public class methodOverloading {
+    public static void main(String args[]){
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(4, 6));
+    }
+}
+
+class Calculator{
+    int sum(int a , int b){
+        return a + b;
+    }
+    float sum(float a, float b){
+        return a + b;
+    }
+    int sum(int a, int b, int c){
+        return a + b + c;
+    }
+}
---
+++ java/methodOverriidng.java
@@ -0,0 +1,17 @@
+public class methodOverriidng {
+    public static void main(String args[]){
+        Deer d = new Deer();
+        d.eat();
+    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("Eat anything");
+    }
+}
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Eat grass");
+    }
+}
